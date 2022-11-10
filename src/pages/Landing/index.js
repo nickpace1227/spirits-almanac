bc17c76530e7c1cecd3ae42ee13f865d1ff4a336
Sam <srsuddath@gmail.com>
2022-11-10 11:37:34 -0500

    add redux example to landing page

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "../store/counterSlice";
 
-export default function LandingPage() {
-  return <div>landing page</div>;
+function LandingPage() {
+  const count = useSelector((store) => store.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="App">
+      <div>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <span>{count}</span>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+      </div>
+    </div>
+  );
 }
+
+export default LandingPage;
