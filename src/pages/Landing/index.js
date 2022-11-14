ef79be371f1d5be823636798c67a8dc5d5995e88
Nick Pace <nicholasppace@gmail.com>
2022-11-14 07:18:54 -0500

    Updated state hooks

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { decrement, increment } from "../../store/counterSlice";
 import { spiritCardSlice } from "../../store/spiritSlice";
 
-function LandingPage() {
+ export default function LandingPage() {
 
   const dispatch = useDispatch();
   const [type, setType] = useState(null);
@@ -11,9 +11,31 @@ function LandingPage() {
   const [name, setName] = useState(null);
   const [proof, setProof] = useState(null);
 
-  const handleClick = (e) => {
+  const handleTypeChange = (e) => {
     const inputValue = e.target.value;
     setType(inputValue);
+    //console.log(type)
+  };
+
+  const handleSubTypeChange = (e) => {
+    const inputValue = e.target.value;
+    setSubType(inputValue);
+    console.log(subType);
+  };
+
+  const handleNameChange = (e) => {
+    const inputValue = e.target.value;
+    setName(inputValue);
+    console.log(name);
+  };
+
+  const handleProofChange = (e) => {
+    const inputValue = e.target.value;
+    setProof(inputValue);
+    console.log(proof);
+  };
+  
+  const handleClick = () => {
     console.log(type);
   //dispatch(spiritCardSlice.addSpiritCard(newCard));
   }
@@ -23,22 +45,19 @@ function LandingPage() {
       <p>Welcome to Spirits Almanac!</p>
       <p>Click below to begin adding bottles to your almanac!</p>
       <form>
-        <input type="text" placeholder="Spirit Type" />
+        <input type="text" placeholder="Spirit Type" onChange={handleTypeChange} />
         <br/>
-        <input type="text" placeholder="Spirit Subtype" />
+        <input type="text" placeholder="Spirit Subtype" onChange={handleSubTypeChange} />
         <br/>
-        <input type="text" placeholder="Spirit Name" />
+        <input type="text" placeholder="Spirit Name" onChange={handleNameChange} />
         <br/>
-        <input type="text" placeholder="Spirit Proof" />
+        <input type="text" placeholder="Spirit Proof" onChange={handleProofChange} />
         <br/>
-        <button onClick={handleClick}>Add Bottle<button/>
+        <button onClick={handleClick}>Submit</button>
       </form>
     </div>
   );
 }
-
-export default LandingPage;
-
 
 //commented out, but saved for later Redux application
 {/* <div>
@@ -46,4 +65,4 @@ export default LandingPage;
 <span>{count}</span>
 <button onClick={() => dispatch(decrement())}>Decrement</button>
 </div> */}
-// const count = useSelector((store) => store.counter.value);
+// const count = useSelector((store) => store.counter.value)
