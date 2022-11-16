ec1a3991bd1ef0671cbcb37ddfa20f635d4428d4
Nick Pace <nicholasppace@gmail.com>
2022-11-16 13:47:27 -0500

    changed some names and worked on redux

--- src/pages/Inventory/index.js
+++ src/pages/Inventory/index.js
@@ -1,9 +1,13 @@
 import React from "react";
+import store from "../../store/cardSlice";
 
 export default function Inventory() {
   return (
     <div>
-      <div>stuff</div>
+      <p>{store.type}</p>
+      <p>{store.subtype}</p>
+      <p>{store.brand}</p>
+      <p>{store.proof}</p>
     </div>
   );
 }
