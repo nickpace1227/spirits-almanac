75eecea8e0c365704ed8933d4e01b03ae0540117
nicholasppace@gmail.com <nicholasppace@gmail.com>
2024-02-13 11:39:16 -0500

    Added logic in router to load local; non-functional right now

--- src/components/Router/index.js
+++ src/components/Router/index.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { GlobalStyles } from "../Styles/GlobalStyles";
+import { GlobalStyles } from "../styles/GlobalStyles";
 import Header from "../Header";
 import LoginPage from "../Login";
 import Almanac from "../Almanac";
@@ -10,6 +10,16 @@ import Contact from "../Contact";
 import CreateUser from "../CreateUser";
 
 export default function SpiritsAlmanac() {
+  const [localAlmanac, setLocalAlmanac] = useState({});
+  const [hasLoaded, setHasLoaded] = useState(false);
+
+  if (!hasLoaded) {
+    const localData = JSON.parse(localStorage.getItem("userData"));
+    setLocalAlmanac(localData);
+    console.log(localAlmanac);
+    setHasLoaded(true);
+  }
+
   return (
     <BrowserRouter>
       <GlobalStyles>
